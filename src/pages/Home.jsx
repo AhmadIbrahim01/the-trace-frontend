dbeff3e0b7ec45efcc3d8434f6a8841f69011515
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-20 22:44:24 +0200

    Edit: HowToJoinSection Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,8 @@ import fingerprint from "../assets/images/fingerprint.png";
 import communityIcon from "../assets/icons/community-icon.svg";
 import rewardIcon from "../assets/icons/reward-icon.svg";
 import justiceIcon from "../assets/icons/justice-icon.svg";
+import howToJoinIcon from "../assets/icons/how-to-join-icon.svg";
+import lines from "../assets/images/lines.svg";
 
 // Action Component for Reusability
 const ActionItem = ({ icon, text }) => (
@@ -71,7 +73,24 @@ const HowToJoinSection = () => (
         Joining us is very simple, just a few steps and you’ll be set up to go!
       </p>
     </div>
-    <div></div>
+    <div className="flex column center">
+      <img className="icon" src={howToJoinIcon} alt="" />
+      <img className="lines" src={lines} alt="" />
+      <div className="cards flex">
+        <div className="card flex column center">
+          <h3 className="t-center">Sign Up and Join the Community</h3>
+        </div>
+        <div className="card flex column center">
+          <h3 className="t-center">
+            Learn about ongoing efforts to solve crimes
+          </h3>
+        </div>
+        <div className="card flex column center">
+          <h3 className="t-center">Share your insights or tips anonymously</h3>
+        </div>
+      </div>
+    </div>
+    <button className="primary-btn">Sign up now</button>
   </div>
 );
 
