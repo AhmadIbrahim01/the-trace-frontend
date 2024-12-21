b3b58e45a154956dcf97d3e3cfdbfbb84a506ec1
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-21 19:54:48 +0200

    Edit: Case Page And Add Headings

--- src/pages/Case/Case.jsx
+++ src/pages/Case/Case.jsx
@@ -3,7 +3,10 @@ import "./Case.css";
 
 const HeroSection = () => (
   <div className="case-hero flex center column">
-    <h1>The Victim Of AI</h1>
+    <div className="case-bio">
+      <h1>The Victim Of AI</h1>
+      <h2>Case #1</h2>
+    </div>
   </div>
 );
 const Case = () => (
