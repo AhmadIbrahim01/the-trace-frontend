ec8b2b6fdb2efa3da1067a800f6ca980096a69cd
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-21 03:15:03 +0200

    Add: Routes To Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import "./App.css";
 import "./styles/base/utilities.css";
 import Home from "./pages/Home";
+import Cases from "./pages/Cases/Cases";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 function App() {
+  const route = createBrowserRouter([
+    { path: "/", element: <Home /> },
+    { path: "/cases", element: <Cases /> },
+  ]);
   return (
     <>
-      <Home />
+      <RouterProvider router={route}></RouterProvider>
     </>
   );
 }
