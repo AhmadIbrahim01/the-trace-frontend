5dcae5899d0bb8100e557c255c313d0171b190cf
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-22 00:42:09 +0200

    Add: Case Description Header

--- src/pages/Case/Case.jsx
+++ src/pages/Case/Case.jsx
@@ -6,6 +6,10 @@ import halfPaper from "../../assets/images/half-paper.svg";
 import caseImage from "../../assets/images/case-image.svg";
 import galleryOne from "../../assets/images/gallery-1.svg";
 import pin from "../../assets/images/pin.svg";
+import sceneTape from "../../assets/images/scene-tape.svg";
+import calendarIcon from "../../assets/icons/calendar-icon.svg";
+import tickCircleIcon from "../../assets/icons/tick-circle-icon.svg";
+import backArrow from "../../assets/icons/back-arrow.svg";
 
 const HeroSection = () => (
   <div className="case-hero flex center column">
@@ -24,14 +28,38 @@ const HeroSection = () => (
     </div>
     <img className="half-paper" src={halfPaper} alt="" />
     <div className="sticky-note flex center">
-      <p>Helllo</p>
+      <p>Case</p>
       <img src={pin} alt="" />
     </div>
   </div>
 );
+
+const CaseDescription = () => (
+  <div className="case-description flex center column">
+    <img className="scene-tape" src={sceneTape} alt="scene-tape" />
+    <div className="description-header flex">
+      <button className="back-btn flex center">
+        <img src={backArrow} alt="" />
+        <p>Back</p>
+      </button>
+      <div className="flex center">
+        <div className="case-status flex center">
+          <p>Case closed</p>
+          <img src={tickCircleIcon} alt="Tick Circle Icon" />
+        </div>
+        <div className="case-date flex">
+          <img src={calendarIcon} alt="Calendar Icon" />
+          <p>Published on January 5, 2023</p>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
 const Case = () => (
   <>
     <HeroSection />
+    <CaseDescription />
   </>
 );
 
