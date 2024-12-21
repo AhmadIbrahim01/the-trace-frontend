a448f45d4e4419224704b5270922d8998fe0ee81
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-21 20:58:55 +0200

    Add: Case Page Hero Section Image Div

--- src/pages/Case/Case.jsx
+++ src/pages/Case/Case.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "./Case.css";
+import tapeOne from "../../assets/images/tape-1.svg";
+import tapeTwo from "../../assets/images/tape-2.svg";
+import halfPaper from "../../assets/images/half-paper.svg";
+import caseImage from "../../assets/images/case-image.svg";
 
 const HeroSection = () => (
   <div className="case-hero flex center column">
@@ -7,6 +11,16 @@ const HeroSection = () => (
       <h1>The Victim Of AI</h1>
       <h2>Case #1</h2>
     </div>
+
+    <div className="image-one">
+      <img src={tapeOne} alt="" />
+      <img src={caseImage} alt="" />
+    </div>
+    <div className="image-two">
+      <img src={tapeTwo} alt="" />
+      <img src="" alt="" />
+    </div>
+    <img className="half-paper" src={halfPaper} alt="" />
   </div>
 );
 const Case = () => (
