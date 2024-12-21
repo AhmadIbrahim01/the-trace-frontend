e0410213a8d4502cc8357f26082cc79a360e20ba
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-21 03:54:30 +0200

    Add: Cases Page Hero Section

--- src/pages/Cases/Cases.jsx
+++ src/pages/Cases/Cases.jsx
@@ -1,5 +1,29 @@
 import React from "react";
+import "./Cases.css";
+import searchIcon from "../../assets/icons/search-icon.svg";
 
-const Cases = () => <></>;
+const HeroSection = () => (
+  <div className="cases-hero flex center column">
+    <h1>Public Cases</h1>
+    <p className="t-center">
+      “I've been using this web hosting service for a few months now and overall
+      it's been fine. The uptime has been good and I haven't had any major
+      issues. The pricing is also reasonable. Nothing particularly stands out
+      as.
+    </p>
+    <div className="search-div flex center">
+      <input type="text" placeholder="Search" />
+      <button>
+        <img src={searchIcon} alt="" />
+      </button>
+    </div>
+  </div>
+);
+
+const Cases = () => (
+  <>
+    <HeroSection />
+  </>
+);
 
 export default Cases;
