4a6c3cd4f6f52cd2f4979e62671928b7600b4767
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-21 03:53:05 +0200

    Edit: Home Page Location

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import "./styles/base/utilities.css";
-import Home from "./pages/Home";
+import Home from "./pages/Home/Home";
 import Cases from "./pages/Cases/Cases";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
