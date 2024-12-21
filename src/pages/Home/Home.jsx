13cdfca5f4be0faec988a1fa64e41fa6af93ee91
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-21 03:54:11 +0200

    Change: Home.jsx And Home.css Location

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,207 @@
+import React from "react";
+import "./Home.css";
+import fingMagn from "../../assets/images/fingerprint-magnifier.png";
+import fingerprint from "../../assets/images/fingerprint.png";
+import communityIcon from "../../assets/icons/community-icon.svg";
+import rewardIcon from "../../assets/icons/reward-icon.svg";
+import justiceIcon from "../../assets/icons/justice-icon.svg";
+import howToJoinIcon from "../../assets/icons/how-to-join-icon.svg";
+import userIcon from "../../assets/icons/user-icon.svg";
+import lines from "../../assets/images/lines.svg";
+import arrow from "../../assets/icons/arrow.svg";
+import magnifier from "../../assets/images/magnifier.svg";
+import papers from "../../assets/images/papers.svg";
+import drawing from "../../assets/images/drawing.svg";
+import house from "../../assets/images/house.svg";
+import whiteFingerprint from "../../assets/images/white-fingerprint.svg";
+
+const ActionItem = ({ icon, text }) => (
+  <div className="action">
+    <img src={icon} alt="" />
+    <h3>{text}</h3>
+  </div>
+);
+
+const TestimonialCard = ({ text, name, image }) => (
+  <div className="testimonial-card">
+    <p>{text}</p>
+    <div className="card-info flex column center">
+      <img src={image} alt="user" />
+      <p>{name}</p>
+    </div>
+  </div>
+);
+
+const testimonials = [
+  {
+    text: `“I've been using this web hosting service for a few months and it's been nothing but problems. My website has gone down multiple times and the customer service has been unresponsive. I would not recommend this company."`,
+    name: "Ahmad Ibrahim",
+    image: userIcon,
+  },
+  {
+    text: `“I've been using this web hosting service for a few months now and overall it's been fine. The uptime has been good and I haven't had any major issues. The pricing is also reasonable. Nothing particularly stands out as exceptional, but it gets the job done.”`,
+    name: "Ahmad Ibrahim",
+    image: userIcon,
+  },
+  {
+    text: `“I've been using this web hosting service for a few months and it's been nothing but problems. My website has gone down multiple times and the customer service has been unresponsive. I would not recommend this company."`,
+    name: "Ahmad Ibrahim",
+    image: userIcon,
+  },
+];
+
+const HeroSection = () => (
+  <div className="hero flex center column">
+    <h1 className="t-center">
+      Let’s keep our city safe <br /> and crime-free
+    </h1>
+    <div className="btn-container flex center">
+      <button className="primary-btn">Login</button>
+      <button className="secondary-btn">View public cases</button>
+    </div>
+  </div>
+);
+
+const GetInvolvedSection = () => (
+  <div className="get-involved flex">
+    <div className="container flex">
+      <div className="container-one flex column">
+        <div className="section-header">
+          <h1>
+            Why get <br /> involved?
+          </h1>
+          <p>Your contribution can make a real difference.</p>
+        </div>
+        <div className="action-list flex wrap">
+          <ActionItem
+            icon={communityIcon}
+            text="Join a justice enthusiast community"
+          />
+          <ActionItem
+            icon={rewardIcon}
+            text="Submit tips and evidence and join our reward system!"
+          />
+          <ActionItem
+            icon={justiceIcon}
+            text="Help to bring justice to your community!"
+          />
+        </div>
+      </div>
+      <div className="container-two">
+        <img src={fingerprint} alt="" />
+        <img src={fingerprint} alt="" />
+        <img src={fingerprint} alt="" />
+      </div>
+    </div>
+    <img className="fingMagn" src={fingMagn} alt="" />
+  </div>
+);
+
+const HowToJoinSection = () => (
+  <div className="how-to-join flex column center">
+    <div className="section-header">
+      <h1 className="t-center">How to join?</h1>
+      <p className="t-center">
+        Joining us is very simple, just a few steps and you’ll be set up to go!
+      </p>
+    </div>
+    <div className="flex column center">
+      <img className="icon" src={howToJoinIcon} alt="" />
+      <img className="lines" src={lines} alt="" />
+      <div className="cards flex">
+        <div className="card flex column center">
+          <h3 className="t-center">Sign Up and Join the Community</h3>
+        </div>
+        <div className="card flex column center">
+          <h3 className="t-center">
+            Learn about ongoing efforts to solve crimes
+          </h3>
+        </div>
+        <div className="card flex column center">
+          <h3 className="t-center">Share your insights or tips anonymously</h3>
+        </div>
+      </div>
+    </div>
+    <button className="primary-btn">Sign up now</button>
+  </div>
+);
+
+const TestimonialsSection = () => (
+  <div className="testimonials flex column center">
+    <div className="testimonials-header flex">
+      <div className="testimonials-info">
+        <h1>Testimonials</h1>
+        <p>
+          Joining us is very simple, just a few steps and you’ll be set up to
+          go!
+        </p>
+      </div>
+      <div className="flex testimonials-btns">
+        <button className="arrow-btn">
+          <img src={arrow} alt="" />
+        </button>
+        <button className="arrow-btn">
+          <img src={arrow} alt="" />
+        </button>
+      </div>
+    </div>
+    <div className="testimonial-cards flex center">
+      {testimonials.map((testimonial, index) => (
+        <TestimonialCard
+          key={index}
+          text={testimonial.text}
+          name={testimonial.name}
+          image={testimonial.image}
+        />
+      ))}
+    </div>
+    <img className="magnifier" src={magnifier} alt="" />
+  </div>
+);
+
+const PoweredByAiSection = () => (
+  <div className="powered flex column center">
+    <h1>
+      Powered by{" "}
+      <span>
+        AI <img src={whiteFingerprint} alt="" />
+      </span>
+    </h1>
+    <p>
+      We highly appreciate technology and AI, by creating AI-driven analysis for
+      solving crimes
+    </p>
+    <div className="flex powered-cards">
+      <div className="big-card">
+        <h2>Statement analysis</h2>
+        <img src={papers} alt="papers" />
+      </div>
+      <div className="small-cards">
+        <div className="small-card flex">
+          <h2>
+            AI drawing <br></br> suspect
+          </h2>
+          <img src={drawing} alt="drawing" />
+        </div>
+        <div className="small-card flex">
+          <h2>
+            3D crime <br /> scene <br /> recreation
+          </h2>
+          <img src={house} alt="house" />
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+const Home = () => (
+  <>
+    <HeroSection />
+    <GetInvolvedSection />
+    <HowToJoinSection />
+    <TestimonialsSection />
+    <PoweredByAiSection />
+  </>
+);
+
+export default Home;
