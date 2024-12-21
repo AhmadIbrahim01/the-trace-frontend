8bd99e694f718e2c6ad78b8e7ea199d5d9348ac7
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-21 17:57:23 +0200

    Add: Case Path

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import "./styles/base/utilities.css";
 import Home from "./pages/Home/Home";
 import Cases from "./pages/Cases/Cases";
+import Case from "./pages/Case/Case";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 function App() {
   const route = createBrowserRouter([
     { path: "/", element: <Home /> },
     { path: "/cases", element: <Cases /> },
+    { path: "/case", element: <Case /> },
   ]);
   return (
     <>
