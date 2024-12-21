03800ab5846cfd4985ad4dac4f9c94f202cfd4bd
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-21 18:14:52 +0200

    Create: Case Page Component With Styling

--- src/pages/Case/Case.jsx
+++ src/pages/Case/Case.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import "./Case.css";
 
-const Case = () => {
-  return <div>Case</div>;
-};
+const HeroSection = () => (
+  <div className="case-hero flex center column">
+    <h1>The Victim Of AI</h1>
+  </div>
+);
+const Case = () => (
+  <>
+    <HeroSection />
+  </>
+);
 
 export default Case;
