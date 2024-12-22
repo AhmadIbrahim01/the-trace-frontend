b0339816a0b9203b42fa9da1d09ac92df0592969
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-23 01:19:34 +0200

    Add: Styling For The Texts Inside Investigator Stats Page

--- src/pages/Investigator/InvestigatorStats/InvestigatorStats.jsx
+++ src/pages/Investigator/InvestigatorStats/InvestigatorStats.jsx
@@ -5,15 +5,15 @@ const InvestigatorStats = () => {
   return (
     <div className="investigator-stats flex column center">
       <h1>Investigator Ahmad Stats</h1>
-      <p>
+      <p className="t-center">
         Here you can find all your stats including number of cases, solved cases
         undergoing cases and much more informations
       </p>
       <div></div>
-      <p>
+      <h3 className="t-center">
         “An investigator's greatest tools are a sharp mind, an observant eye,
         and an unyielding determination to uncover the truth.”
-      </p>
+      </h3>
     </div>
   );
 };
