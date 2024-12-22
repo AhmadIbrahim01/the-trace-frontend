aef22cdfce4c68357f108da3aa8b5af5018435ec
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-23 01:08:39 +0200

    Add: Investigator Stats Page Header

--- src/pages/Investigator/InvestigatorStats/InvestigatorStats.jsx
+++ src/pages/Investigator/InvestigatorStats/InvestigatorStats.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const InvestigatorStats = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Investigator Ahmad Stats</h1>
+      <p>
+        Here you can find all your stats including number of cases, solved cases
+        undergoing cases and much more informations
+      </p>
+      <div></div>
+      <p>
+        “An investigator's greatest tools are a sharp mind, an observant eye,
+        and an unyielding determination to uncover the truth.”
+      </p>
+    </div>
+  );
 };
 
 export default InvestigatorStats;
