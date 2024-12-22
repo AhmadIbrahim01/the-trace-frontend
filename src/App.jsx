0303fd92de3784a354fa13bb06baf9c0cd82f08e
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-22 23:46:32 +0200

    Add: All News Page Path

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home/Home";
 import Cases from "./pages/Cases/Cases";
 import Case from "./pages/Case/Case";
 import News from "./pages/News/News";
+import AllNews from "./pages/AllNews/AllNews";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     { path: "/cases", element: <Cases /> },
     { path: "/case", element: <Case /> },
     { path: "/news", element: <News /> },
+    { path: "/allnews", element: <AllNews /> },
   ]);
   return (
     <>
