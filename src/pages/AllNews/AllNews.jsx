1df191c2bc072cb4845179a9dea6ca3150139b2a
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-22 23:49:37 +0200

    Add: News Container Section To The All News Page

--- src/pages/AllNews/AllNews.jsx
+++ src/pages/AllNews/AllNews.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./AllNews.css";
+import news from "../../assets/images/news.png";
 
 const HeroSection = () => (
   <div className="all-news-hero flex column">
@@ -13,10 +14,81 @@ const HeroSection = () => (
   </div>
 );
 
+const NewsContainer = ({ title, data }) => {
+  return (
+    <div className="news-container flex center column">
+      <div className="news-container-header">
+        <h1 className="t-center">{title}</h1>
+      </div>
+      <div className="news-cards flex wrap center">
+        {data.map((d, index) => (
+          <div key={index} className="news-card flex center column">
+            <div className="img-container flex">
+              {d.link ? (
+                <a href={d.link} className="flex news-link">
+                  <img src={d.img} alt={d.description} />
+                </a>
+              ) : (
+                <img src={d.img} alt={d.description} />
+              )}
+            </div>
+            <p>{d.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
 const News = () => {
+  const data1 = [
+    {
+      img: news,
+      description: "Israeli forces begin withdrawal from southern Lebanon",
+      link: "https://example.com/article1",
+    },
+    {
+      img: news,
+      description: "Israeli forces begin withdrawal from southern Lebanon",
+      link: "https://example.com/article1",
+    },
+    {
+      img: news,
+      description: "Israeli forces begin withdrawal from southern Lebanon",
+      link: "https://example.com/article1",
+    },
+    {
+      img: news,
+      description: "Israeli forces begin withdrawal from southern Lebanon",
+      link: "https://example.com/article1",
+    },
+    {
+      img: news,
+      description: "Israeli forces begin withdrawal from southern Lebanon",
+      link: "https://example.com/article1",
+    },
+    {
+      img: news,
+      description: "Israeli forces begin withdrawal from southern Lebanon",
+      link: "https://example.com/article1",
+    },
+    {
+      img: news,
+      description: "Israeli forces begin withdrawal from southern Lebanon",
+      link: "https://example.com/article1",
+    },
+    {
+      img: news,
+      description: "Israeli forces begin withdrawal from southern Lebanon",
+      link: "https://example.com/article1",
+    },
+  ];
+
   return (
     <>
       <HeroSection />
+
+      <NewsContainer title="Latest News" data={data1} />
     </>
   );
 };
