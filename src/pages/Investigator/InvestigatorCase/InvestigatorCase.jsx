d2a1350eaae62a5f741bbcbc873c20901ec8974d
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-23 23:04:16 +0200

    Edit: Investigator Case Structure

--- src/pages/Investigator/InvestigatorCase/InvestigatorCase.jsx
+++ src/pages/Investigator/InvestigatorCase/InvestigatorCase.jsx
@@ -4,7 +4,17 @@ const InvestigatorCase = () => {
   return (
     <div className="investigator-case flex column center">
       <div className="inevstigator-case-header">
-        <div className="case-evidence"></div>
+        <div className="case-evidence-container flex center column">
+          <h3>Evidence</h3>
+          <div className="case-evidences flex">
+            <div className="case-evidence flex center column">
+              <button className="case-evidence-button">
+                <img className="case-evidence-img" src="" alt="" />
+              </button>
+            </div>
+          </div>
+          <div className="case-evidences"></div>
+        </div>
         <div className="case-suspects"></div>
       </div>
       <div className="inevstigator-case-body">
