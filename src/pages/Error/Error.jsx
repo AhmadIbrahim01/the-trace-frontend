ba1c6510b598aa28bc1646ff8b2ca14e6c56ece8
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-23 16:23:02 +0200

    Add: Images To Error Page

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import fingerPrint from "../../assets/images/caribbean-fingerprint.svg";
 import "./Error.css";
 import backArrow from "../../assets/icons/back-arrow.svg";
+import magnifierTwo from "../../assets/images/white-magnifier.svg";
 
 const Error = () => {
   return (
@@ -16,6 +17,11 @@ const Error = () => {
         <img src={backArrow} alt="Back arrow" />
         <p>Back to home</p>
       </button>
+      <img src={magnifierTwo} className="magni1" alt="" />
+      <img src={magnifierTwo} className="magni2" alt="" />
+      <img src={magnifierTwo} className="magni3" alt="" />
+      <img src={magnifierTwo} className="magni4" alt="" />
+      <img src={magnifierTwo} className="magni5" alt="" />
     </div>
   );
 };
