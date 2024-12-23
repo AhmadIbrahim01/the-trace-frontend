ff57a3b40dbee59bedd7cf8b0a53b6473368c651
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-23 22:59:06 +0200

    Add: Investigator Case Initial Structure

--- src/pages/Investigator/InvestigatorCase/InvestigatorCase.jsx
+++ src/pages/Investigator/InvestigatorCase/InvestigatorCase.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const InvestigatorCase = () => {
-  return <div>InvestigatorCase</div>;
+  return (
+    <div className="investigator-case flex column center">
+      <div className="inevstigator-case-header">
+        <div className="case-evidence"></div>
+        <div className="case-suspects"></div>
+      </div>
+      <div className="inevstigator-case-body">
+        <div className="case-map"></div>
+        <div className="case-statements"></div>
+      </div>
+    </div>
+  );
 };
 
 export default InvestigatorCase;
