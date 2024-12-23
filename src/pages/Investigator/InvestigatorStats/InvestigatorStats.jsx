39488b2ca86417d1e233428e5b0d12ed7f6d1d84
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-23 03:16:42 +0200

    Add: Pictures For The Investigator Stats Page

--- src/pages/Investigator/InvestigatorStats/InvestigatorStats.jsx
+++ src/pages/Investigator/InvestigatorStats/InvestigatorStats.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./InvestigatorStats.css";
+import magnifierTwo from "../../../assets/images/magnifier-2.svg";
+import caribbeanFingerprint from "../../../assets/images/caribbean-fingerprint.svg";
 
 const InvestigatorStats = () => {
   const data = {
@@ -13,39 +15,43 @@ const InvestigatorStats = () => {
   const { total, solved, resolution, undergoing, cold } = data;
 
   return (
-    <div className="investigator-stats flex column center">
-      <h1>Investigator Ahmad Stats</h1>
-      <p className="t-center">
-        Here you can find all your stats including number of cases, solved cases
-        undergoing cases and much more informations
-      </p>
-      <div class="investigator-grid">
-        <div class="div1 flex column center">
-          <p>Total Case Handled</p>
-          <h1>{total}</h1>
+    <>
+      <div className="investigator-stats flex column center">
+        <h1>Investigator Ahmad Stats</h1>
+        <p className="t-center">
+          Here you can find all your stats including number of cases, solved
+          cases undergoing cases and much more informations
+        </p>
+        <div class="investigator-grid">
+          <div class="div1 flex column center">
+            <p>Total Case Handled</p>
+            <h1>{total}</h1>
+          </div>
+          <div class="div2 flex column center">
+            <p>Solved Cases</p>
+            <h1>{solved}</h1>
+          </div>
+          <div class="div3 flex column center">
+            <p>Case Resolution Rate</p>
+            <h1>{resolution}%</h1>
+            <img src={caribbeanFingerprint} alt="" />
+          </div>
+          <div class="div4 flex column center">
+            <p>Undergoing Cases</p>
+            <h1>{undergoing}</h1>
+          </div>
+          <div class="div5 flex column center">
+            <p>Cold Cases</p>
+            <h1>{cold}</h1>
+          </div>
         </div>
-        <div class="div2 flex column center">
-          <p>Solved Cases</p>
-          <h1>{solved}</h1>
-        </div>
-        <div class="div3 flex column center">
-          <p>Case Resolution Rate</p>
-          <h1>{resolution}%</h1>
-        </div>
-        <div class="div4 flex column center">
-          <p>Undergoing Cases</p>
-          <h1>{undergoing}</h1>
-        </div>
-        <div class="div5 flex column center">
-          <p>Cold Cases</p>
-          <h1>{cold}</h1>
-        </div>
+        <h3 className="t-center">
+          “An investigator's greatest tools are a sharp mind, an observant eye,
+          and an unyielding determination to uncover the truth.”
+        </h3>
       </div>
-      <h3 className="t-center">
-        “An investigator's greatest tools are a sharp mind, an observant eye,
-        and an unyielding determination to uncover the truth.”
-      </h3>
-    </div>
+      <img className="magnifier-two" src={magnifierTwo} alt="" />
+    </>
   );
 };
 
