bb94a770d7b42b0d3a24a3e94b8ebc6e51e25a9f
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-23 16:04:32 +0200

    Add: Error Page Structure

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import fingerPrint from "../../assets/images/caribbean-fingerprint.svg";
+import "./Error.css";
+import backArrow from "../../assets/icons/back-arrow.svg";
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <div className="error flex column center">
+      <h1>Page Not Found</h1>
+      <div className="error404 flex center">
+        <h1>4</h1>
+        <img src={fingerPrint} alt="" />
+        <h1>4</h1>
+      </div>
+      <button className="flex center">
+        <img src={backArrow} alt="Back arrow" />
+        <p>Back to home</p>
+      </button>
+    </div>
+  );
 };
 
 export default Error;
