3fa093e784c44fd6f07ec519e3ecd95db3e65542
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-25 00:52:43 +0200

    Add: Select Evidence Inside Add Evidence Form

--- src/pages/Investigator/InvestigatorForms/AddEvidence/AddEvidence.jsx
+++ src/pages/Investigator/InvestigatorForms/AddEvidence/AddEvidence.jsx
@@ -1,14 +1,27 @@
 import React, { useState } from "react";
-import Input from "../Input/Input";
-import Button from "../Button/Button";
+import Input from "../../../../components/Input/Input";
+import Button from "../../../../components/Button/Button";
+import "./AddEvidence.css";
 
-const FormExample = () => {
+const AddEvidence = () => {
   return (
-    <div className="investigator-form-container">
+    <div className="investigator-form-container t-center flex column center">
       <h1>Add Evidence</h1>
-      <form onSubmit={onSubmit}></form>
+      <form>
+        <div className="input flex column">
+          <label className="t-left" htmlFor="evidences">
+            Type of evidence
+          </label>
+          <select name="evidences" id="evidences">
+            <option value="fingerprint">Fingerprint</option>
+            <option value="file">File</option>
+            <option value="hair">Hair</option>
+            <option value="other">Other</option>
+          </select>
+        </div>
+      </form>
     </div>
   );
 };
 
-export default FormExample;
+export default AddEvidence;
