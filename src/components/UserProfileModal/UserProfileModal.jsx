f6b25faa2a778acd36d6a6642a1091d03487fcf8
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-24 15:10:56 +0200

    Add: User Modal Profile Structure

---
+++ src/components/UserProfileModal/UserProfileModal.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./UserProfileModal.css";
+
+const Modal = ({ isOpen, onClose, data }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="user-modal-overlay" onClick={onClose}>
+      <div className="user-modal-content" onClick={(e) => e.stopPropagation()}>
+        <h2>Popup Content</h2>
+        <p>{data}</p>
+        <button onClick={onClose}>Close</button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
