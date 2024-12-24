9574ffd7aa54b2656f1144f1181824937de95bec
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-24 22:46:08 +0200

    Edit: Input Component Structure

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-const Input = () => {
-  return <div>Input</div>;
+const Input = ({ id, label, type, value, onChange, placeholder }) => {
+  return (
+    <div className="input-container">
+      <label htmlFor={id}>{label}</label>
+      <input
+        id={id}
+        type={type}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+      />
+    </div>
+  );
 };
 
 export default Input;
