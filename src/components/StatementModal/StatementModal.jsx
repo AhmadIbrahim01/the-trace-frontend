3ec7d56b9b9a102d7617a837b5d6a3fbfe5e1468
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-24 13:43:03 +0200

    Add: Statements Inside Statement Modal

--- src/components/StatementModal/StatementModal.jsx
+++ src/components/StatementModal/StatementModal.jsx
@@ -3,11 +3,15 @@ import "./StatementModal.css";
 
 const StatementModal = ({ isOpen, onClose, data }) => {
   if (!isOpen) return null;
+  console.log(data);
 
   return (
     <div className="modal-overlay" onClick={onClose}>
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-        <h2>Popup Content</h2>
+        <div className="suspect-profile-header flex center">
+          <h2>Statement {1}</h2>
+          <button>Edit Information</button>
+        </div>
         <p>{data}</p>
         <button onClick={onClose}>Close</button>
       </div>
