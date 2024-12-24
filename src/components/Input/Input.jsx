80c076d02b327306a98986c5165c09a687d29b53
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-24 23:01:04 +0200

    Fix: Error In Input Component

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-const Input = ({ id, label, type, value, onChange, placeholder }) => {
+const Input = ({ id, name, label, type, value, onChange, placeholder }) => {
   return (
     <div className="input-container">
       <label htmlFor={id}>{label}</label>
       <input
         id={id}
+        name={name}
         type={type}
         value={value}
         onChange={onChange}
