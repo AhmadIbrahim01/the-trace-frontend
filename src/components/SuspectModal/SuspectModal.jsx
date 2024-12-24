3e317f18a445752f81eecdc802f469988ea249ec
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-24 04:11:09 +0200

    Add: Structure For Suspect Profile Modal

--- src/components/SuspectModal/SuspectModal.jsx
+++ src/components/SuspectModal/SuspectModal.jsx
@@ -14,7 +14,65 @@ const Modal = ({ isOpen, onClose, data }) => {
   return (
     <div className="modal-overlay" onClick={onClose}>
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-        <h2>Popup Content</h2>
+        <div className="suspect-profile-header">
+          <h2>Profile</h2>
+          <button>Edit Information</button>
+        </div>
+        <div className="suspect-profile-body">
+          <img src="" alt="" />
+          <div>
+            <div>
+              <img src="" alt="" />
+              <p>Name: John Doe</p>
+            </div>
+            <div>
+              <img src="" alt="" />
+              <p>Age: 23</p>
+            </div>
+            <div>
+              <img src="" alt="" />
+              <p>Occupation: Sales manager</p>
+            </div>
+            <div>
+              <img src="" alt="" />
+              <p>Blood: O+</p>
+            </div>
+            <div>
+              <img src="" alt="" />
+              <p>Eye color/Hair: Light brown/Brown </p>
+            </div>
+            <div>
+              <img src="" alt="" />
+              <p>Height/Weight: 175cm/77kg</p>
+            </div>
+          </div>
+          <button>
+            <img src="" alt="" />
+          </button>
+        </div>
+
+        <div className="suspect-profile-body">
+          <div>
+            <div>
+              <img src="" alt="" />
+              <p>Height/Weight: 175cm/77kg</p>
+            </div>
+            <button>View on map</button>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <p>Phone: +96176468212</p>
+          </div>
+          <div>
+            <h3>images:</h3>
+            <div>
+              <img src="" alt="" />
+              <img src="" alt="" />
+              <img src="" alt="" />
+            </div>
+          </div>
+        </div>
+
         <p>{suspects[data - 1].name}</p>
         <button onClick={onClose}>Close</button>
       </div>
