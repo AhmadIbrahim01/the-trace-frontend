0b6ae47e461cdc857cc5ed1801861a6aa7ec0081
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-24 15:22:12 +0200

    Edit: User Profile Modal Structure

--- src/components/UserProfileModal/UserProfileModal.jsx
+++ src/components/UserProfileModal/UserProfileModal.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import "./UserProfileModal.css";
+import suspect from "../../assets/images/suspect.svg";
 
 const Modal = ({ isOpen, onClose, data }) => {
   if (!isOpen) return null;
 
+  const { author, time, text, likes, profileImage } = data;
+
   return (
     <div className="user-modal-overlay" onClick={onClose}>
       <div className="user-modal-content" onClick={(e) => e.stopPropagation()}>
-        <h2>Popup Content</h2>
-        <p>{data}</p>
-        <button onClick={onClose}>Close</button>
+        <div className="user-profile-header flex center">
+          <h2>Profile</h2>
+          <button>Edit</button>
+        </div>
+        <div className="user-profile-image flex column center">
+          <button>
+            <img src={suspect} alt="" />
+          </button>
+          <h2>{author}</h2>
+          <button>
+            <img src="" alt="" />
+          </button>
+        </div>
       </div>
     </div>
   );
