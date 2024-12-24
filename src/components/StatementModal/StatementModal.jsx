649bed795a25c68376c636e74bc344fa41a5dacc
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-24 14:01:08 +0200

    Add: Statements Header Inside Of Statement Modal

--- src/components/StatementModal/StatementModal.jsx
+++ src/components/StatementModal/StatementModal.jsx
@@ -1,17 +1,32 @@
 import React from "react";
 import "./StatementModal.css";
+import suspect from "../../assets/images/suspect.svg";
+import calendar from "../../assets/icons/calendar.svg";
 
 const StatementModal = ({ isOpen, onClose, data }) => {
   if (!isOpen) return null;
   console.log(data);
 
   return (
-    <div className="modal-overlay" onClick={onClose}>
-      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+    <div className="statement-modal-overlay" onClick={onClose}>
+      <div
+        className="statement-modal-content"
+        onClick={(e) => e.stopPropagation()}
+      >
         <div className="suspect-profile-header flex center">
           <h2>Statement {1}</h2>
           <button>Edit Information</button>
         </div>
+        <button className="statement-modal-profile flex">
+          <img src={suspect} className="statement-modal-image" />
+          <div className="statement-info flex column">
+            <h4 className="t-left">Ahmad Ibrahim</h4>
+            <div className="statement-modal-date t-left flex center">
+              <img src={calendar} alt="" />
+              <p> Given on January 5, 2023</p>
+            </div>
+          </div>
+        </button>
         <p>{data}</p>
         <button onClick={onClose}>Close</button>
       </div>
