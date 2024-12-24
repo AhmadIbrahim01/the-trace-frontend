3d9f8e8c0dba49d198846ec28984b0c850f70206
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-24 04:27:31 +0200

    Add: Icons For Suspect Modal Profile

--- src/components/SuspectModal/SuspectModal.jsx
+++ src/components/SuspectModal/SuspectModal.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 import "./SuspectModal.css";
+import suspect from "../../assets/images/suspect.svg";
+import user from "../../assets/icons/user.svg";
+import calendar from "../../assets/icons/calendar.svg";
+import briefcase from "../../assets/icons/briefcase.svg";
+import drop from "../../assets/icons/drop.svg";
+import eye from "../../assets/icons/eye.svg";
+import ruler from "../../assets/icons/ruler.svg";
+import location from "../../assets/icons/location.svg";
+import call from "../../assets/icons/call.svg";
 
 const Modal = ({ isOpen, onClose, data }) => {
   if (!isOpen) return null;
@@ -14,35 +23,35 @@ const Modal = ({ isOpen, onClose, data }) => {
   return (
     <div className="modal-overlay" onClick={onClose}>
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-        <div className="suspect-profile-header">
+        <div className="suspect-profile-header flex center">
           <h2>Profile</h2>
           <button>Edit Information</button>
         </div>
         <div className="suspect-profile-body">
-          <img src="" alt="" />
+          <img src={suspect} alt="" />
           <div>
             <div>
-              <img src="" alt="" />
+              <img src={user} alt="" />
               <p>Name: John Doe</p>
             </div>
             <div>
-              <img src="" alt="" />
+              <img src={calendar} alt="" />
               <p>Age: 23</p>
             </div>
             <div>
-              <img src="" alt="" />
+              <img src={briefcase} alt="" />
               <p>Occupation: Sales manager</p>
             </div>
             <div>
-              <img src="" alt="" />
+              <img src={drop} alt="" />
               <p>Blood: O+</p>
             </div>
             <div>
-              <img src="" alt="" />
+              <img src={eye} alt="" />
               <p>Eye color/Hair: Light brown/Brown </p>
             </div>
             <div>
-              <img src="" alt="" />
+              <img src={ruler} alt="" />
               <p>Height/Weight: 175cm/77kg</p>
             </div>
           </div>
