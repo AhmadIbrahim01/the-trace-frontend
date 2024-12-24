3c1f5f7bc086a2fd4319b952faf4e0f82f99352c
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-24 03:35:18 +0200

    Edit: Suspect Modal To Make It More Dynamic

--- src/components/SuspectModal/SuspectModal.jsx
+++ src/components/SuspectModal/SuspectModal.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-import "./Modal.css";
+import "./SuspectModal.css";
 
 const Modal = ({ isOpen, onClose, data }) => {
   if (!isOpen) return null;
+  console.log(data);
+
+  const suspects = [
+    { id: 1, name: "Adnan Ibrahim" },
+    { id: 2, name: "Adnan Ibrahim" },
+    { id: 3, name: "Adnan Ibrahim" },
+  ];
 
   return (
     <div className="modal-overlay" onClick={onClose}>
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
         <h2>Popup Content</h2>
-        <p>{data}</p>
+        <p>{suspects[data - 1].name}</p>
         <button onClick={onClose}>Close</button>
       </div>
     </div>
