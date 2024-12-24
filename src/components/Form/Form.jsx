6da9a6c801b45434bf92475194268b0a080fccee
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-24 23:06:07 +0200

    Edit: Form Component

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import Input from "../Input/Input";
+import Button from "../Button/Button";
 
-const Form = () => {
-  return <div>Form</div>;
+const FormExample = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div>
+        <Input
+          id="username"
+          name="username"
+          label="Username"
+          type="text"
+          value={formData.username}
+          onChange={handleInputChange}
+          placeholder="Enter your username"
+        />
+      </div>
+
+      <div>
+        <Input
+          id="email"
+          name="email"
+          label="Email"
+          type="email"
+          value={formData.email}
+          onChange={handleInputChange}
+          placeholder="Enter your email"
+        />
+      </div>
+
+      <Button type="submit" text="Submit"></Button>
+    </form>
+  );
 };
 
-export default Form;
+export default FormExample;
