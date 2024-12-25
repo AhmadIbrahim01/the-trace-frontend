bf72f5433b4ad98dab248221d8e7266d1b30e6ad
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-26 00:08:09 +0200

    Add: Basic Structure For AI Sketch Page

--- src/pages/Tools/AISketch/AISketch.jsx
+++ src/pages/Tools/AISketch/AISketch.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import Input from "../../../components/Input/Input";
+import Button from "../../../components/Button/Button";
+import "./AISketch.css";
 
 const AISketch = () => {
-  return <div>AISketch</div>;
+  return (
+    <div className="sketch flex center">
+      <form className="sketch-form flex center column">
+        <h1>Suspect Details</h1>
+
+        <div className="sketch-input flex column">
+          <label htmlFor="name">Name</label>
+          <input
+            id="name"
+            name="name"
+            type="text"
+            placeholder="Enter suspect name"
+          />
+        </div>
+      </form>
+      <div className="sketch-container flex column center">
+        <h1>AI Sketch</h1>
+        <div></div>
+        <button>Save Sketch</button>
+      </div>
+    </div>
+  );
 };
 
 export default AISketch;
