83c344b7490222145191c99d3ba753879bce3a0d
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-25 21:31:06 +0200

    Add: Path To Manage Cases Page

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ import AddCase from "./pages/Admin/AdminForms/AddCase/AddCase";
 import AddUser from "./pages/Admin/AdminForms/AddUser/AddUser";
 import AdminDashboard from "./pages/Admin/AdminDashboard/Dashboard/Dashboard";
 import ManageInvestigators from "./pages/Admin/AdminDashboard/ManageInvestigators/ManageInvestigators";
+import ManageCases from "./pages/Admin/AdminDashboard/ManageCases/ManageCases";
 
 function App() {
   const route = createBrowserRouter([
@@ -42,6 +43,7 @@ function App() {
     { path: "/add-user", element: <AddUser /> },
     { path: "/admin-dashboard", element: <AdminDashboard /> },
     { path: "/manage-investigators", element: <ManageInvestigators /> },
+    { path: "/manage-cases", element: <ManageCases /> },
   ]);
   return (
     <>
