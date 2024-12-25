9937d60504dadb6a3917d01fba86ff82c2f423d8
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-25 02:09:45 +0200

    Add: Header Inside Add Statement Page

---
+++ src/pages/Investigator/InvestigatorForms/AddStatement/AddStatement.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Input from "../../../../components/Input/Input";
+import Button from "../../../../components/Button/Button";
+import "./AddStatement.css";
+
+const AddEvidence = () => {
+  return (
+    <div className="investigator-form-container t-center flex column center">
+      <h1>Add Statement</h1>
+    </div>
+  );
+};
+
+export default AddEvidence;
