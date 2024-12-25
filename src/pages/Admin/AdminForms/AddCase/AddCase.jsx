5252e492beb05a2a3ac8405fe5b9c2c2b1ce23b6
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-25 15:22:43 +0200

    Add: Case Title Input Inside Add Case Page

--- src/pages/Admin/AdminForms/AddCase/AddCase.jsx
+++ src/pages/Admin/AdminForms/AddCase/AddCase.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import Input from "../../../../components/Input/Input";
+import Button from "../../../../components/Button/Button";
+import "./AddCase.css";
 
 const AddCase = () => {
   return (
     <div className="admin-form-container t-center flex column center">
       <h1>Add Case</h1>
+      <form>
+        <Input
+          id={"case"}
+          label={"Case Title"}
+          name={"case"}
+          type={"text"}
+        ></Input>
+      </form>
     </div>
   );
 };
