a1e14ba26f9c496ddf714e654782d348b3205d63
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-25 15:00:59 +0200

    Add: Full Name Input Inside Add Investigator Page

--- src/pages/Admin/AdminForms/AddInvestigator.jsx
+++ src/pages/Admin/AdminForms/AddInvestigator.jsx
@@ -1,9 +1,19 @@
 import React from "react";
-
+import Input from "../../../components/Input/Input";
+import Button from "../../../components/Button/Button";
+import "./AddInvestigator.css";
 const AddInvestigator = () => {
   return (
     <div className="investigator-form-container t-center flex column center">
       <h1>Add Investigator</h1>
+      <form>
+        <Input
+          id={"name"}
+          label={"Full Name"}
+          name={"name"}
+          type={"text"}
+        ></Input>
+      </form>
     </div>
   );
 };
