00553de06d8f2f658c20afaa5c15754e5eacc75c
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-25 02:10:28 +0200

    Add: Input For Witness Name Inside Of Add Statement Page

--- src/pages/Investigator/InvestigatorForms/AddStatement/AddStatement.jsx
+++ src/pages/Investigator/InvestigatorForms/AddStatement/AddStatement.jsx
@@ -7,6 +7,14 @@ const AddEvidence = () => {
   return (
     <div className="investigator-form-container t-center flex column center">
       <h1>Add Statement</h1>
+      <form>
+        <Input
+          id={"witness"}
+          label={"Witness Name"}
+          name={"witness"}
+          type={"text"}
+        ></Input>
+      </form>
     </div>
   );
 };
