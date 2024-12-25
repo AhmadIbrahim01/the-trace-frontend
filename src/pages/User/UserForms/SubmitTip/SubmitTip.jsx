c770ffbdd7b91b4dec55e6cc2fc3f6a4d0d0ac35
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-25 04:19:20 +0200

    Add: Header For Submit Tip Page

--- src/pages/User/UserForms/SubmitTip/SubmitTip.jsx
+++ src/pages/User/UserForms/SubmitTip/SubmitTip.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const SubmitTip = () => {
-  return <div>SubmitTip</div>;
+  return (
+    <div className="investigator-form-container t-center flex column center">
+      <h1>Submit a Tip?</h1>
+    </div>
+  );
 };
 
 export default SubmitTip;
