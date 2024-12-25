2679c7598101449f38f3b220f059d29e5e77ce96
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-25 20:02:50 +0200

    Add: Sidebar Inside Manage Investigators Page

--- src/pages/Admin/AdminDashboard/ManageInvestigators/ManageInvestigators.jsx
+++ src/pages/Admin/AdminDashboard/ManageInvestigators/ManageInvestigators.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import "./ManageInvestigators.css";
+import adminImage from "../../../../assets/images/suspect.svg";
+import dashboardIconOne from "../../../../assets/icons/dashboard-icon.svg";
+import dashboardIconTwo from "../../../../assets/icons/dashboard-icon-2.svg";
 
 const ManageInvestigators = () => {
-  return <div>ManageInvestigators</div>;
+  return (
+    <div className="admin-dashboard flex">
+      <div className="admin-sidebar flex column center">
+        <button className="admin-profile flex column center">
+          <img src={adminImage} alt="" />
+          <h1>Admin Name</h1>
+        </button>
+        <ul className="dashboard-ul flex center column">
+          <li className="dashboard-li">
+            <button>
+              <img src={dashboardIconOne} alt="" />
+              Dashboard
+            </button>
+          </li>
+          <li className="dashboard-li">
+            <button>
+              <img src={dashboardIconTwo} alt="" />
+              Manage Investigators
+            </button>
+          </li>
+          <li className="dashboard-li">
+            <button>
+              <img src={dashboardIconTwo} alt="" />
+              Manage Cases
+            </button>
+          </li>
+          <li className="dashboard-li">
+            <button>
+              <img src={dashboardIconTwo} alt="" />
+              Manage Tools
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default ManageInvestigators;
