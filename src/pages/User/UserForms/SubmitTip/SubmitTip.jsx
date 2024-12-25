232a24ba848abaa887f0fee19b46b326da6b8aca
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-25 04:21:07 +0200

    Add: Text Area For Tip Information Inside Submit Tip Page

--- src/pages/User/UserForms/SubmitTip/SubmitTip.jsx
+++ src/pages/User/UserForms/SubmitTip/SubmitTip.jsx
@@ -4,6 +4,17 @@ const SubmitTip = () => {
   return (
     <div className="investigator-form-container t-center flex column center">
       <h1>Submit a Tip?</h1>
+      <form>
+        <div className="input flex column">
+          <label htmlFor="statement">Tip Details</label>
+          <textarea
+            id="statement"
+            placeholder="Describe the information you want to share..."
+            name="statement"
+            rows={10}
+          ></textarea>
+        </div>
+      </form>
     </div>
   );
 };
