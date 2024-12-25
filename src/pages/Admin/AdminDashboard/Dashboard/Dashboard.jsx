576e40548132c1bf419f517e7b65e4eab7ba79c2
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-25 17:20:18 +0200

    Add: Dashboard Sidebar Profile

--- src/pages/Admin/AdminDashboard/Dashboard/Dashboard.jsx
+++ src/pages/Admin/AdminDashboard/Dashboard/Dashboard.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import "./Dashboard.css";
+import adminImage from "../../../../assets/images/suspect.svg";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="admin-dashboard flex">
+      <div className="admin-sidebar flex column center">
+        <div className="admin-profile flex column center">
+          <img src={adminImage} alt="" />
+          <h1>Admin Name</h1>
+        </div>
+        <ul>
+          <li>
+            <button>
+              <img src="" alt="" />
+              Dashboard
+            </button>
+          </li>
+          <li>
+            <button>
+              <img src="" alt="" />
+              Manage Investigators
+            </button>
+          </li>
+        </ul>
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Dashboard;
