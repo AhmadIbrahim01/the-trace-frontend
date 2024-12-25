37c298124eb7c754b780abde3ddba3f7255d1676
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-25 02:37:19 +0200

    Add: Witness Address Input Inside Add Witness Page

--- src/pages/Investigator/InvestigatorForms/AddWitness/AddWitness.jsx
+++ src/pages/Investigator/InvestigatorForms/AddWitness/AddWitness.jsx
@@ -28,6 +28,12 @@ const AddWitness = () => {
             <option value="female">Female</option>
           </select>
         </div>
+        <Input
+          id={"address"}
+          label={"Address"}
+          name={"address"}
+          type={"text"}
+        ></Input>
       </form>
     </div>
   );
