a029900f15d09efd741783db0322fe72840e23f0
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-25 15:46:17 +0200

    Add: Header Inside Add User Page

--- src/pages/Admin/AdminForms/AddUser/AddUser.jsx
+++ src/pages/Admin/AdminForms/AddUser/AddUser.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Input from "../../../../components/Input/Input";
+import Button from "../../../../components/Button/Button";
+import "./AddUser.css";
 
 const AddUser = () => {
-  return <div>AddUser</div>;
+  return (
+    <div className="admin-form-container t-center flex column center">
+      <h1>Add User</h1>
+    </div>
+  );
 };
 
 export default AddUser;
