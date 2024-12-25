324baf2b3c705e4bf3bf0f8eeeeb5a0ced1cdf2b
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-25 14:59:03 +0200

    Add: Header Inside Add Investigator Page

--- src/pages/Admin/AdminForms/AddInvestigator.jsx
+++ src/pages/Admin/AdminForms/AddInvestigator.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const AddInvestigator = () => {
-  return <div>AddInvestigator</div>;
+  return (
+    <div className="investigator-form-container t-center flex column center">
+      <h1>Add Investigator</h1>
+    </div>
+  );
 };
 
 export default AddInvestigator;
