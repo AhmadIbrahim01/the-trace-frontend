9ed1eb729e7e2e37ccc0b1d202d5437a5eac0131
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-25 02:47:58 +0200

    Add: Header For Add Suspect Page

--- src/pages/Investigator/InvestigatorForms/AddSuspect/AddSuspect.jsx
+++ src/pages/Investigator/InvestigatorForms/AddSuspect/AddSuspect.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import Input from "../../../../components/Input/Input";
+import Button from "../../../../components/Button/Button";
+import "./AddSuspect.css";
 
 const AddSuspect = () => {
-  return <div>AddSuspect</div>;
+  return (
+    <div className="investigator-form-container t-center flex column center">
+      <h1>Add Evidence</h1>
+    </div>
+  );
 };
 
 export default AddSuspect;
