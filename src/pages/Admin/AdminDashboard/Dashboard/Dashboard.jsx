6782ae116ce3b8b68fb6c6eef0601a9b7814b491
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-25 21:59:37 +0200

    Edit: Dashboard Page Sidebar Buttons

--- src/pages/Admin/AdminDashboard/Dashboard/Dashboard.jsx
+++ src/pages/Admin/AdminDashboard/Dashboard/Dashboard.jsx
@@ -3,6 +3,7 @@ import "./Dashboard.css";
 import adminImage from "../../../../assets/images/suspect.svg";
 import dashboardIconOne from "../../../../assets/icons/dashboard-icon.svg";
 import dashboardIconTwo from "../../../../assets/icons/dashboard-icon-2.svg";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
   const adminDashboardStats = {
@@ -14,6 +15,20 @@ const Dashboard = () => {
 
   const { active, investigators, tools, resolvedCases } = adminDashboardStats;
 
+  const navigate = useNavigate();
+  const navigateToDashboard = () => {
+    navigate("/admin-dashboard");
+  };
+  const navigateToInvestigators = () => {
+    navigate("/manage-investigators");
+  };
+  const navigateToUsers = () => {
+    navigate("/manage-users");
+  };
+  const navigateToCases = () => {
+    navigate("/manage-cases");
+  };
+
   return (
     <div className="admin-dashboard flex">
       <div className="admin-sidebar flex column center">
@@ -22,29 +37,19 @@ const Dashboard = () => {
           <h1>Admin Name</h1>
         </button>
         <ul className="dashboard-ul flex center column">
-          <li className="dashboard-li">
-            <button>
-              <img src={dashboardIconOne} alt="" />
-              Dashboard
-            </button>
+          <li className="dashboard-li dashboard-li-clicked">
+            <button onClick={navigateToDashboard}>Dashboard</button>
           </li>
           <li className="dashboard-li">
-            <button>
-              <img src={dashboardIconTwo} alt="" />
+            <button onClick={navigateToInvestigators}>
               Manage Investigators
             </button>
           </li>
           <li className="dashboard-li">
-            <button>
-              <img src={dashboardIconTwo} alt="" />
-              Manage Cases
-            </button>
+            <button onClick={navigateToCases}>Manage Cases</button>
           </li>
           <li className="dashboard-li">
-            <button>
-              <img src={dashboardIconTwo} alt="" />
-              Manage Tools
-            </button>
+            <button onClick={navigateToUsers}>Manage Users</button>
           </li>
         </ul>
       </div>
