0dee1ed37f8cc8f519d0be0cab9699b078c8f590
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-25 17:36:02 +0200

    Add: Dasboard Sidebar Buttons With Styling

--- src/pages/Admin/AdminDashboard/Dashboard/Dashboard.jsx
+++ src/pages/Admin/AdminDashboard/Dashboard/Dashboard.jsx
@@ -1,26 +1,40 @@
 import React from "react";
 import "./Dashboard.css";
 import adminImage from "../../../../assets/images/suspect.svg";
+import dashboardIconOne from "../../../../assets/icons/dashboard-icon.svg";
+import dashboardIconTwo from "../../../../assets/icons/dashboard-icon-2.svg";
 
 const Dashboard = () => {
   return (
     <div className="admin-dashboard flex">
       <div className="admin-sidebar flex column center">
-        <div className="admin-profile flex column center">
+        <button className="admin-profile flex column center">
           <img src={adminImage} alt="" />
           <h1>Admin Name</h1>
-        </div>
-        <ul>
-          <li>
+        </button>
+        <ul className="dashboard-ul flex center column">
+          <li className="dashboard-li">
             <button>
-              <img src="" alt="" />
+              <img src={dashboardIconOne} alt="" />
               Dashboard
             </button>
           </li>
-          <li>
+          <li className="dashboard-li">
             <button>
-              <img src="" alt="" />
+              <img src={dashboardIconTwo} alt="" />
               Manage Investigators
+            </button>
+          </li>
+          <li className="dashboard-li">
+            <button>
+              <img src={dashboardIconTwo} alt="" />
+              Manage Cases
+            </button>
+          </li>
+          <li className="dashboard-li">
+            <button>
+              <img src={dashboardIconTwo} alt="" />
+              Manage Tools
             </button>
           </li>
         </ul>
