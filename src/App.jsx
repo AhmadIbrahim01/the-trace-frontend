c0960a091395ed51ad2f0917a57aa450e88a808e
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-26 04:50:46 +0200

    Edit: App.jsx Route

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import AllNews from "./pages/AllNews/AllNews";
 import InvStats from "./pages/Investigator/InvestigatorStats/InvestigatorStats";
 import InvCase from "./pages/Investigator/InvestigatorCase/InvestigatorCase";
 import Error from "./pages/Error/Error";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Route, RouterProvider, createBrowserRouter } from "react-router-dom";
 import Form from "./components/Form/Form";
 import AddEvidence from "./pages/Investigator/InvestigatorForms/AddEvidence/AddEvidence";
 import AddStatement from "./pages/Investigator/InvestigatorForms/AddStatement/AddStatement";
@@ -24,32 +24,39 @@ import ManageCases from "./pages/Admin/AdminDashboard/ManageCases/ManageCases";
 import ManageUsers from "./pages/Admin/AdminDashboard/ManageUsers/ManageUsers";
 import AISketch from "./pages/Tools/AISketch/AISketch";
 import AIStatement from "./pages/Tools/AIStatement/AIStatement";
+import Layout from "./components/Layout/Layout";
 
 function App() {
   const route = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/cases", element: <Cases /> },
+        { path: "/case", element: <Case /> },
+        { path: "/news", element: <News /> },
+        { path: "/allnews", element: <AllNews /> },
+        { path: "/investigator-stats", element: <InvStats /> },
+        { path: "/investigator-case", element: <InvCase /> },
+        { path: "/form", element: <Form /> },
+        { path: "/add-evidence", element: <AddEvidence /> },
+        { path: "/add-statement", element: <AddStatement /> },
+        { path: "/add-witness", element: <AddWitness /> },
+        { path: "/add-suspect", element: <AddSuspect /> },
+        { path: "/submit-tip", element: <SubmitTip /> },
+        { path: "/add-investigator", element: <AddInvestigator /> },
+        { path: "/add-case", element: <AddCase /> },
+        { path: "/add-user", element: <AddUser /> },
+        { path: "/admin-dashboard", element: <AdminDashboard /> },
+        { path: "/manage-investigators", element: <ManageInvestigators /> },
+        { path: "/manage-cases", element: <ManageCases /> },
+        { path: "/manage-users", element: <ManageUsers /> },
+        { path: "/ai-sketch", element: <AISketch /> },
+        { path: "/ai-statement", element: <AIStatement /> },
+      ],
+    },
     { path: "*", element: <Error /> },
-    { path: "/", element: <Home /> },
-    { path: "/cases", element: <Cases /> },
-    { path: "/case", element: <Case /> },
-    { path: "/news", element: <News /> },
-    { path: "/allnews", element: <AllNews /> },
-    { path: "/investigator-stats", element: <InvStats /> },
-    { path: "/investigator-case", element: <InvCase /> },
-    { path: "/form", element: <Form /> },
-    { path: "/add-evidence", element: <AddEvidence /> },
-    { path: "/add-statement", element: <AddStatement /> },
-    { path: "/add-witness", element: <AddWitness /> },
-    { path: "/add-suspect", element: <AddSuspect /> },
-    { path: "/submit-tip", element: <SubmitTip /> },
-    { path: "/add-investigator", element: <AddInvestigator /> },
-    { path: "/add-case", element: <AddCase /> },
-    { path: "/add-user", element: <AddUser /> },
-    { path: "/admin-dashboard", element: <AdminDashboard /> },
-    { path: "/manage-investigators", element: <ManageInvestigators /> },
-    { path: "/manage-cases", element: <ManageCases /> },
-    { path: "/manage-users", element: <ManageUsers /> },
-    { path: "/ai-sketch", element: <AISketch /> },
-    { path: "/ai-statement", element: <AIStatement /> },
   ]);
   return (
     <>
