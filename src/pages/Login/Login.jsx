13e7b95fccc9340c0bbd8d30e3bc7ba9e4ef295e
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-26 19:47:41 +0200

    Add: Login Email Input Inside Of Login Page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import Input from "../../components/Input/Input";
+import Button from "../../components/Button/Button";
+import "./Login.css";
+import GoogleIcon from "../../assets/icons/google-icon.svg";
+import logo from "../../assets/images/logo.svg";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  const goToRegister = () => {
+    navigate("/register");
+  };
+  return (
+    <div className="register flex center column">
+      <Link to="/" className="register-logo">
+        <img src={logo} alt="" />
+      </Link>
+      <form className="register-form flex center column">
+        <h1>Login</h1>
+        <h3>Welcome back!</h3>
+
+        <div className="register-input flex column">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            placeholder="ex. ahmad@gmail.com"
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
