4539b871dea5ed62b933708a6d53b2ec5a53de7a
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-26 15:29:02 +0200

    Edit: Buttons Inside User Edit profile Modal

--- src/components/userEditProfileModal/userEditProfileModal.jsx
+++ src/components/userEditProfileModal/userEditProfileModal.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./UserEditProfileModal.css";
 import Input from "../Input/Input";
+import Button from "../Button/Button";
 
 const userEditProfileModal = ({ isOpen, onClose, data }) => {
   if (!isOpen) return null;
@@ -30,8 +31,18 @@ const userEditProfileModal = ({ isOpen, onClose, data }) => {
             <label htmlFor="password">Password</label>
             <input id="password" name="password" type="password" />
           </div>
+          <div className="edit-input flex column">
+            <label htmlFor="file">Profile Photo</label>
+            <input id="file" name="file" type="file" />
+          </div>
         </form>
-        <button onClick={onClose}>Close</button>
+        <div className="edit-user-buttons flex">
+          <Button
+            className={"edit-user-button flex center"}
+            text={"Save Changes"}
+          ></Button>
+          <button onClick={onClose}>Close</button>
+        </div>
       </div>
     </div>
   );
