40f010bd56db6587bd95d646142e04f0300c69ba
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-26 17:46:47 +0200

    Add: Header For Register Page

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Input from "../../components/Input/Input";
+import Button from "../../components/Button/Button";
+import "./Register.css";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="register flex center column">
+      <img src="" alt="" />
+      <form className="register-form flex center column">
+        <h1>Register</h1>
+        <h3>Join us now!</h3>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
