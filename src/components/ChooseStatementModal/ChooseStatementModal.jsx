476a1a39a6929df7388dea35bfe33dcc2d6ed9d8
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-28 16:01:32 +0200

    Edit: Choose Statement Modal To Have Paths For Suspect And Witness Statemen Add

--- src/components/ChooseStatementModal/ChooseStatementModal.jsx
+++ src/components/ChooseStatementModal/ChooseStatementModal.jsx
@@ -1,26 +1,37 @@
 import React from "react";
-import "./chooseStatemnt.css";
+import "./ChooseStatementModal.css";
 import { useNavigate } from "react-router-dom";
 
 const chooseStatemnt = ({ isOpen, onClose, data }) => {
   if (!isOpen) return null;
   const navigate = useNavigate();
-  suspectOnClick = () => {
-    navigate("/add-suspect-statemnet");
+  const suspectOnClick = () => {
+    navigate("/add-suspect-statement");
   };
-  witnessOnClick = () => {
-    navigate("/add-witness-statemnet");
+  const witnessOnClick = () => {
+    navigate("/add-witness-statement");
   };
 
   return (
-    <div className="modal-overlay" onClick={onClose}>
-      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-        <h2>Choose Statement</h2>
-        <div className="flex">
-          <button onClick={suspectOnClick}>Suspect Statement</button>
-          <button onClick={witnessOnClick}>Witness Statement</button>
+    <div className="statement-modal-overlay" onClick={onClose}>
+      <div
+        className="statement-modal-content"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className="suspect-profile-header flex center">
+          <h2>Choose Statement</h2>
         </div>
-        <button onClick={onClose}>Close</button>
+        <div className="choose-btns flex">
+          <button className="choose-btn" onClick={suspectOnClick}>
+            Suspect Statement
+          </button>
+          <button className="choose-btn" onClick={witnessOnClick}>
+            Witness Statement
+          </button>
+        </div>
+        <button onClick={onClose} className="close-btn flex center column">
+          Close
+        </button>
       </div>
     </div>
   );
