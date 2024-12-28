3b42d64d11898cccae23a70e97c6565977822e14
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-28 15:43:13 +0200

    Edit: Choose Statemnt Modal

--- src/components/ChooseStatementModal/ChooseStatementModal.jsx
+++ src/components/ChooseStatementModal/ChooseStatementModal.jsx
@@ -1,18 +1,29 @@
 import React from "react";
-import "./Modal.css";
+import "./chooseStatemnt.css";
+import { useNavigate } from "react-router-dom";
 
-const Modal = ({ isOpen, onClose, data }) => {
+const chooseStatemnt = ({ isOpen, onClose, data }) => {
   if (!isOpen) return null;
+  const navigate = useNavigate();
+  suspectOnClick = () => {
+    navigate("/add-suspect-statemnet");
+  };
+  witnessOnClick = () => {
+    navigate("/add-witness-statemnet");
+  };
 
   return (
     <div className="modal-overlay" onClick={onClose}>
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-        <h2>Popup Content</h2>
-        <p>{data}</p>
+        <h2>Choose Statement</h2>
+        <div className="flex">
+          <button onClick={suspectOnClick}>Suspect Statement</button>
+          <button onClick={witnessOnClick}>Witness Statement</button>
+        </div>
         <button onClick={onClose}>Close</button>
       </div>
     </div>
   );
 };
 
-export default Modal;
+export default chooseStatemnt;
