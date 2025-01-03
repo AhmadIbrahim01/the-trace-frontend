8a94fb705380f75d415f3868ac4f16669cbf841d
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2025-01-03 18:42:22 +0200

    Add: Fetch User Data Inside User Profile Page

--- src/pages/User/UserProfile/UserProfile.jsx
+++ src/pages/User/UserProfile/UserProfile.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import profilePicture from "../../../assets/images/suspect.svg";
+import profile from "../../../assets/images/suspect.svg";
 import document from "../../../assets/icons/document.svg";
 import video from "../../../assets/icons/video.svg";
 import gallery from "../../../assets/icons/gallery.svg";
@@ -11,41 +11,110 @@ import starOne from "../../../assets/icons/star-1.svg";
 import starTwo from "../../../assets/icons/star-2.svg";
 
 import UserEditProfileModal from "../../../components/UserEditProfileModal/UserEditProfileModal";
+import axios from "axios";
+import { jwtDecode } from "jwt-decode";
 
 const UserProfile = () => {
-  const [isEditModalOpen, setEditModalOpen] = useState(false);
-  const [editModalData, setEditModalData] = useState("");
-  const data = {
-    author: "Ahmad",
-    time: "123",
-    text: "asdadas",
-    likes: 132,
-    profileImage: "123",
-  };
-  const { author, time, text, likes, profileImage } = data;
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  const [userId, setUserId] = useState(null);
 
-  const openEditModal = (data) => {
-    setEditModalData(data);
-    setEditModalOpen(true);
-  };
-
-  const closeEditModal = () => {
-    setEditModalOpen(false);
-  };
   const navigate = useNavigate();
 
   const logOut = () => {
     localStorage.clear();
     navigate("/login");
   };
+
   const goBack = () => {
     navigate(-1);
   };
 
+  const [isEditModalOpen, setEditModalOpen] = useState(false);
+  const [editModalData, setEditModalData] = useState("");
+
+  const openEditModal = (data) => {
+    setEditModalData(user);
+    setEditModalOpen(true);
+  };
+
+  const closeEditModal = () => {
+    setEditModalOpen(false);
+  };
+
   const [progress, setProgress] = useState(30);
 
+  const token = localStorage.getItem("authToken");
+
+  useEffect(() => {
+    if (token) {
+      try {
+        const decodedToken = jwtDecode(token);
+        const currentTime = Date.now() / 1000;
+        if (decodedToken.exp < currentTime) {
+          setError("Token expired");
+          localStorage.removeItem("authToken");
+        } else {
+          setUserId(decodedToken.userId);
+        }
+      } catch (err) {
+        console.error("Failed to decode token:", err);
+        setError("Failed to decode token");
+      }
+    } else {
+      setError("No token found");
+    }
+  }, [token]);
+
+  useEffect(() => {
+    if (userId) {
+      const fetchUser = async () => {
+        setLoading(true);
+        try {
+          const response = await axios.get(
+            `http://127.0.0.1:8080/api/auth/${userId}`,
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+          setUser(response.data.user);
+          setLoading(false);
+        } catch (error) {
+          console.log(error.message);
+          setError("Failed to load user data");
+          setLoading(false);
+        }
+      };
+      fetchUser();
+    }
+  }, [userId, token]);
+  console.log(user);
+
+  const {
+    firstName,
+    lastName,
+    email,
+    level,
+    phone,
+    profilePicture,
+    tipsStats,
+    badges,
+  } = user;
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+  console.log(badges);
+
   return (
-    <div className=" flex center column">
+    <div className="flex center column">
       <button type="button" className="logout-btn" onClick={logOut}>
         Log Out
       </button>
@@ -56,56 +125,56 @@ const UserProfile = () => {
       <div className="user-modal-content" onClick={(e) => e.stopPropagation()}>
         <div className="user-modal-header flex center">
           <h2>Profile</h2>
-          <button onClick={() => openEditModal(data)}>Edit</button>
+          <button onClick={() => openEditModal(user)}>Edit</button>
         </div>
         <div className="user-profile-image flex column center">
           <button className="user-profile-button">
-            <img src={profilePicture} alt="" />
+            <img src={profilePicture || profile} alt="" />
           </button>
           <button className="user-profile-edit">
             <img src={edit} alt="" />
           </button>
-          <h2>Ahmad</h2>
+          <h2>{firstName}</h2>
         </div>
         <div className="user-modal-header flex center">
-          <h2>Level 3</h2>
+          <h2>Level {level}</h2>
         </div>
         <div className="user-progress">
           <p>30/60</p>
           <div className="progress-bar">
             <div className="progress" style={{ width: `${progress}%` }}></div>
           </div>
-          <p>30 more comments till level 4</p>
+          <p>30 more comments till level </p>
         </div>
         <div className="user-modal-header flex column">
           <h2>Submitted tips</h2>
           <div className="submitted-tips flex center">
             <div className="submitted-tip flex center">
               <img src={video} alt="" />
-              <h3>3 Videos</h3>
+              <h3>{tipsStats.videos} Videos</h3>
             </div>
             <div className="submitted-tip flex center">
               <img src={gallery} alt="" />
-              <h3>3 Photos</h3>
+              <h3>{tipsStats.photos} Photos</h3>
             </div>
             <div className="submitted-tip flex center">
               <img src={document} alt="" />
-              <h3>3 Docs</h3>
+              <h3>{tipsStats.documents} Docs</h3>
             </div>
           </div>
-          <p className="accepted-tips t-left">4 accepted tips</p>
+          <p className="accepted-tips t-left">
+            {tipsStats.accepted} accepted tips
+          </p>
         </div>
         <div className="user-modal-header flex column">
           <h2>Badges</h2>
           <div className="badges flex center wrap">
-            <div className="badge flex center">
-              <img src={starOne} alt="" />
-              <h3>Top contributor</h3>
-            </div>
-            <div className="badge flex center">
-              <img src={starTwo} alt="" />
-              <h3>Top commenter</h3>
-            </div>
+            {badges.map((badge, index) => (
+              <div key={index} className="badge flex center">
+                <img src={starOne} />
+                <h3>{badge}</h3>
+              </div>
+            ))}
           </div>
         </div>
       </div>
