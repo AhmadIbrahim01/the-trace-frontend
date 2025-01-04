5ba8310f0281c91dd41d4dd749ed6bce29943b1e
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2025-01-04 20:46:03 +0200

    Add: Map Container And Tile Layer

--- src/components/MapComponent/MapComponent.jsx
+++ src/components/MapComponent/MapComponent.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
 
 const MapComponent = () => {
-  return <div>MapComponent</div>;
+  return (
+    <MapContainer center={[48.8566, 2.3522]} zoom={20}>
+      <TileLayer
+        attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
+  );
 };
 
 export default MapComponent;
