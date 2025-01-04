2994ced705cf4c068005625ce0b678652c3000aa
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2025-01-04 04:25:57 +0200

    Edit: Cases Page To Fetch Cases Data

--- src/pages/Cases/Cases.jsx
+++ src/pages/Cases/Cases.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Cases.css";
 import searchIcon from "../../assets/icons/search-icon.svg";
 import caseOne from "../../assets/images/case1.svg";
@@ -9,6 +9,7 @@ import calendarIcon from "../../assets/icons/calendar-icon.svg";
 import arrow from "../../assets/icons/arrow-2.svg";
 import tickCircleIcon from "../../assets/icons/tick-circle-icon.svg";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const HeroSection = () => (
   <div className="cases-hero flex center column">
@@ -28,21 +29,33 @@ const HeroSection = () => (
   </div>
 );
 
-const CaseCard = ({ caseData }) => {
+const CaseCard = ({ caseData, index }) => {
   const navigate = useNavigate();
   const goToCase = () => {
     navigate("/case");
   };
 
+  const date = new Date(caseData.createdAt);
+
+  const formattedDate = date.toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+
+  console.log(caseData.caseImages);
+
   return (
     <div className="case">
-      <p className="t-center flex center">{caseData.description}</p>
+      <p className="t-center flex center">
+        {Array(20).fill(caseData.title).join(" ")}
+      </p>
       <div className="case-container flex">
         <div className="case-image">
-          <img src={caseData.image || caseOne} alt={caseData.title} />
+          <img src={caseData.caseImages[0] || caseOne} alt={caseData.title} />
         </div>
         <div className="case-info">
-          <p>Case {caseData.id}</p>
+          <p>Case {index + 1}</p>
           <div className="case-name flex">
             <h2>{caseData.title}</h2>
             <button className="flex center">
@@ -51,7 +64,7 @@ const CaseCard = ({ caseData }) => {
           </div>
           <div className="case-date flex">
             <img src={calendarIcon} alt="Calendar Icon" />
-            <p>Published on {caseData.date}</p>
+            <p>Published on {formattedDate}</p>
           </div>
           <div className="tags flex">
             {caseData.tags.map((tag, index) => (
@@ -73,38 +86,59 @@ const CaseCard = ({ caseData }) => {
 };
 
 const CasesSection = () => {
-  const cases = [
-    {
-      id: 1,
-      title: "The Victim of AI",
-      description:
-        "The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI ",
-      date: "January 5, 2023",
-      tags: ["Tag One", "Tag Two", "Tag Three"],
-      status: "Case closed",
-      image: caseOne,
-    },
-    {
-      id: 2,
-      title: "The Victim of AI",
-      description:
-        "The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI ",
-      date: "January 5, 2023",
-      tags: ["Tag One", "Tag Two", "Tag Three"],
-      status: "Case closed",
-      image: caseTwo,
-    },
-    {
-      id: 3,
-      title: "Hitman in the night",
-      description:
-        "The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI ",
-      date: "January 5, 2023",
-      tags: ["Tag One", "Tag Two", "Tag Three"],
-      status: "Case closed",
-      image: caseThree,
-    },
-  ];
+  // const cases = [
+  //   {
+  //     id: 1,
+  //     title: "The Victim of AI",
+  //     description:
+  //       "The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI ",
+  //     date: "January 5, 2023",
+  //     tags: ["Tag One", "Tag Two", "Tag Three"],
+  //     status: "Case closed",
+  //     image: caseOne,
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "The Victim of AI",
+  //     description:
+  //       "The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI ",
+  //     date: "January 5, 2023",
+  //     tags: ["Tag One", "Tag Two", "Tag Three"],
+  //     status: "Case closed",
+  //     image: caseTwo,
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "Hitman in the night",
+  //     description:
+  //       "The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI The Victim of AI ",
+  //     date: "January 5, 2023",
+  //     tags: ["Tag One", "Tag Two", "Tag Three"],
+  //     status: "Case closed",
+  //     image: caseThree,
+  //   },
+  // ];
+
+  const [cases, setC] = useState([]);
+
+  useEffect(() => {
+    const getCases = async () => {
+      try {
+        const response = await axios.get("http://127.0.0.1:8080/api/case/", {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        setC(response.data.cases);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getCases();
+  }, []);
+
+  console.log(cases);
 
   return (
     <div className="cases-list flex center column">
@@ -112,9 +146,21 @@ const CasesSection = () => {
       <p className="t-center">
         Joining us is very simple, just a few steps and you’ll be set up to go!
       </p>
-      {cases.map((caseData) => (
-        <CaseCard key={caseData.id} caseData={caseData} />
-      ))}
+      {cases.length !== 0 ? (
+        cases.map((caseData, index) => (
+          <CaseCard key={index} index={index} caseData={caseData} />
+        ))
+      ) : (
+        <>
+          <h1 style={{ color: "red" }}>No available cases</h1>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+        </>
+      )}
     </div>
   );
 };
