5e6dcae5ba04fa30e31ab8484a4e0bfe31bde532
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2025-01-04 21:16:58 +0200

    Add: Marker To The Map

--- src/components/MapComponent/MapComponent.jsx
+++ src/components/MapComponent/MapComponent.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import "leaflet/dist/leaflet.css";
 import "./MapComponent.css";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { Icon } from "leaflet";
 
+import pin from "../../assets/images/pin.svg";
 const MapComponent = ({ latitude, longitude }) => {
+  const customIcon = new Icon({
+    iconUrl: pin,
+    iconSize: [50, 50],
+  });
+
   return (
     <MapContainer
       center={[latitude || 33.89031080600027, longitude || 35.47024942065306]}
@@ -13,6 +20,14 @@ const MapComponent = ({ latitude, longitude }) => {
         attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+
+      <Marker
+        position={[
+          latitude || 33.89031080600027,
+          longitude || 35.47024942065306,
+        ]}
+        icon={customIcon}
+      ></Marker>
     </MapContainer>
   );
 };
