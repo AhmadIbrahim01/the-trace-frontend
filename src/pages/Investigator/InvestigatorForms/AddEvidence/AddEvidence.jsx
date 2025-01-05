55b83fd5c805a1b6e323d5f332b2da2f4ae105fc
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2025-01-06 01:15:37 +0200

    Add: Functionality To Add Evidence Page

--- src/pages/Investigator/InvestigatorForms/AddEvidence/AddEvidence.jsx
+++ src/pages/Investigator/InvestigatorForms/AddEvidence/AddEvidence.jsx
@@ -1,18 +1,58 @@
 import React, { useState } from "react";
-import Input from "../../../../components/Input/Input";
 import Button from "../../../../components/Button/Button";
 import "./AddEvidence.css";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useForm } from "react-hook-form";
 
 const AddEvidence = () => {
   const navigate = useNavigate();
   const goBack = () => {
     navigate(-1);
   };
+  const [status, setStatus] = useState({ success: true, message: "" });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const caseId = localStorage.getItem("caseId");
+
+  const onSubmit = async (data) => {
+    try {
+      const response = await axios.post(
+        `http://127.0.0.1:8080/api/evidence/${caseId}`,
+        data,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      setStatus({
+        success: true,
+        message: "Login successfull",
+      });
+
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.message);
+
+      const errorMessage =
+        error.response?.data?.message || "Something went wrong";
+      setStatus({ success: false, message: errorMessage });
+    }
+    reset();
+  };
+
   return (
     <div className="investigator-form-container t-center flex column center">
       <h1>Add Evidence</h1>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <Button
           name={"back"}
           text={"Back to manage case"}
@@ -22,7 +62,7 @@ const AddEvidence = () => {
         ></Button>
         <div className="input flex column">
           <label htmlFor="evidences">Type of Evidence</label>
-          <select name="evidences" id="evidences">
+          <select name="type" id="type" {...register("type")}>
             <option value="fingerprint">Fingerprint</option>
             <option value="file">File</option>
             <option value="hair">Hair</option>
@@ -31,27 +71,40 @@ const AddEvidence = () => {
         </div>
         <div className="input flex column">
           <label htmlFor="description">Description</label>
-          <textarea id="description" name="description"></textarea>
+          <textarea
+            id="description"
+            name="description"
+            {...register("description")}
+          ></textarea>
         </div>
 
-        <Input
-          id={"location"}
-          label={"Location of Collection"}
-          name={"location"}
-          type={"text"}
-        ></Input>
-        <Input
-          id={"date"}
-          label={"Date Collected"}
-          name={"date"}
-          type={"date"}
-        ></Input>
-        <Input
-          id={"file"}
-          label={"Evidence Image"}
-          name={"file"}
-          type={"file"}
-        ></Input>
+        <div className="input flex column">
+          <label htmlFor={"location"}>Location of Collection</label>
+          <input
+            id={"location"}
+            name={"location"}
+            type={"text"}
+            {...register("location")}
+          />
+        </div>
+        <div className="input flex column">
+          <label htmlFor={"collectedAt"}>Date Collected</label>
+          <input
+            id={"collectedAt"}
+            name={"collectedAt"}
+            type={"date"}
+            {...register("collectedAt")}
+          />
+        </div>
+        <div className="input flex column">
+          <label htmlFor={"photo"}>Evidence Image</label>
+          <input
+            id={"photo"}
+            name={"photo"}
+            type={"file"}
+            {...register("photo")}
+          />
+        </div>
 
         <Button
           type={"submit"}
