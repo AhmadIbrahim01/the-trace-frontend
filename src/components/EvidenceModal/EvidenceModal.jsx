d05e4eb118a0c8508cc8b366e2740044a27f6f49
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2025-01-05 20:18:27 +0200

    Edit: Evidence Modal To Show Evidence Data

--- src/components/EvidenceModal/EvidenceModal.jsx
+++ src/components/EvidenceModal/EvidenceModal.jsx
@@ -4,12 +4,35 @@ import "./EvidenceModal.css";
 const EvidenceModal = ({ isOpen, onClose, data }) => {
   if (!isOpen) return null;
 
+  const { type, collectedAt, description, location, photo } = data[0];
+
+  const date = new Date(collectedAt);
+
   return (
-    <div className="modal-overlay" onClick={onClose}>
-      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-        <h2>Popup Content</h2>
-        <p>{data}</p>
-        <button onClick={onClose}>Close</button>
+    <div className="statement-modal-overlay" onClick={onClose}>
+      <div
+        className="statement-modal-content scrollable-div"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className="suspect-profile-header flex center">
+          <h2>Evidence</h2>
+          <button>Edit Information</button>
+        </div>
+        <img src={photo} className="flex center column evidence-image" />
+        <div className="suspect-profile-body">
+          <div className="suspect-data flex">
+            <p>Type: {type}</p>
+          </div>
+          <div className="suspect-data flex">
+            <p>Description: {description}</p>
+          </div>
+          <div className="suspect-data flex">
+            <p>Location: {location}</p>
+          </div>
+          <div className="suspect-data flex">
+            <p>Collected data: {date.toString()}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
