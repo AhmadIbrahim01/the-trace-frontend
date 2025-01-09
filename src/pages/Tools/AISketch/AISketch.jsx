019f01656f8e57d00cdbe80f55ab7f8ce4475442
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2025-01-09 03:34:37 +0200

    Edit: AI Sketch To Switch Between Sketch Input Or Text Input

--- src/pages/Tools/AISketch/AISketch.jsx
+++ src/pages/Tools/AISketch/AISketch.jsx
@@ -6,6 +6,10 @@ import { useForm } from "react-hook-form";
 import axios from "axios";
 
 const AISketch = () => {
+  const [sketch, setSketch] = useState(true);
+  const change = () => {
+    setSketch(!sketch);
+  };
   const [imageUrl, setImageUrl] = useState("");
   const [status, setStatus] = useState({
     success: true,
@@ -57,106 +61,139 @@ const AISketch = () => {
   }
 
   return (
-    <div className="sketch flex center">
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="sketch-form flex center column"
-      >
-        <h1>Suspect Details</h1>
-
-        <div className="sketch-input flex column">
-          <label htmlFor="name">Name</label>
-          <input
-            id="name"
-            type="text"
-            placeholder="Enter suspect name"
-            {...register("name", { required: "Name is required" })}
-          />
-          {errors.name && <p style={{ color: "red" }}>{errors.name.message}</p>}
-        </div>
-
-        <div className="sketch-input flex column">
-          <label htmlFor="age">Suspect Approximated Age</label>
-          <select id="age" {...register("age")}>
-            <option value="less than 18">Younger than 18</option>
-            <option value="18-25">18-25</option>
-            <option value="26-35">26-35</option>
-            <option value="36-45">36-45</option>
-            <option value="46-55">46-55</option>
-            <option value="56-65">56-65</option>
-            <option value="66-75">66-75</option>
-            <option value="older than 75">Older than 75</option>
-          </select>
-        </div>
-        <div className="sketch-input flex column">
-          <label htmlFor="description">Description</label>
-          <textarea
-            id="description"
-            placeholder="Provide a detailed description"
-            rows={10}
-            {...register("description", {
-              required: "Description is required",
-            })}
-          ></textarea>
-          {errors.description && (
-            <p style={{ color: "red" }}>{errors.description.message}</p>
-          )}
-        </div>
-
-        <div className="sketch-input flex column">
-          <label htmlFor="additional">Additional Features</label>
-          <textarea
-            id="additional"
-            placeholder="e.g., scars, tattoos, hair type"
-            rows={10}
-            {...register("additional")}
-          ></textarea>
-        </div>
-
-        {/* <div className="sketch-input sketch-input-file flex column">
-          <label htmlFor="photo">Upload Photo</label>
-          <input id="photo" type="file" {...register("photo")} />
-        </div> */}
-
-        <Button
-          type={"submit"}
-          name={"generate-sketch"}
-          text={"Generate Sketch"}
-          className={"ai-form-button"}
-        ></Button>
-        {status.message && status.success === true ? (
-          <h3 style={{ color: "green", marginBottom: "30px" }}>
-            {status.message}
-          </h3>
-        ) : (
-          <h3 style={{ color: "red", marginBottom: "30px" }}>
-            {status.message}
-          </h3>
-        )}
-      </form>
-
-      <div className="sketch-container flex column center">
-        <h1>AI Sketch</h1>
-        {imageUrl && (
-          <>
-            <div className="ai-sketch">
-              <img src={imageUrl} alt="" />
+    <>
+      <div className="sketch flex center">
+        {sketch ? (
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="sketch-form flex center column"
+          >
+            <h1>Suspect Drawn Picture</h1>
+            <h3 className="switch-btn1" onClick={change}>
+              Switch to text input
+            </h3>
+            <div className="sketch-input sketch-input-file flex column">
+              <label htmlFor="photo">Upload Photo</label>
+              <input id="photo" type="file" {...register("photo")} />
             </div>
+
             <Button
               type={"submit"}
-              name={"save-sketch"}
-              text={"Save Sketch"}
+              name={"generate-sketch"}
+              text={"Generate Sketch"}
               className={"ai-form-button"}
             ></Button>
-          </>
+            {status.message && status.success === true ? (
+              <h3 style={{ color: "green", marginBottom: "30px" }}>
+                {status.message}
+              </h3>
+            ) : (
+              <h3 style={{ color: "red", marginBottom: "30px" }}>
+                {status.message}
+              </h3>
+            )}
+          </form>
+        ) : (
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="sketch-form flex center column"
+          >
+            <h1>Suspect Details</h1>
+            <h3 className="switch-btn2" onClick={change}>
+              Switch to image input
+            </h3>
+            <div className="sketch-input flex column">
+              <label htmlFor="name">Name</label>
+              <input
+                id="name"
+                type="text"
+                placeholder="Enter suspect name"
+                {...register("name", { required: "Name is required" })}
+              />
+              {errors.name && (
+                <p style={{ color: "red" }}>{errors.name.message}</p>
+              )}
+            </div>
+
+            <div className="sketch-input flex column">
+              <label htmlFor="age">Suspect Approximated Age</label>
+              <select id="age" {...register("age")}>
+                <option value="less than 18">Younger than 18</option>
+                <option value="18-25">18-25</option>
+                <option value="26-35">26-35</option>
+                <option value="36-45">36-45</option>
+                <option value="46-55">46-55</option>
+                <option value="56-65">56-65</option>
+                <option value="66-75">66-75</option>
+                <option value="older than 75">Older than 75</option>
+              </select>
+            </div>
+            <div className="sketch-input flex column">
+              <label htmlFor="description">Description</label>
+              <textarea
+                id="description"
+                placeholder="Provide a detailed description"
+                rows={10}
+                {...register("description", {
+                  required: "Description is required",
+                })}
+              ></textarea>
+              {errors.description && (
+                <p style={{ color: "red" }}>{errors.description.message}</p>
+              )}
+            </div>
+
+            <div className="sketch-input flex column">
+              <label htmlFor="additional">Additional Features</label>
+              <textarea
+                id="additional"
+                placeholder="e.g., scars, tattoos, hair type"
+                rows={10}
+                {...register("additional")}
+              ></textarea>
+            </div>
+
+            <Button
+              type={"submit"}
+              name={"generate-sketch"}
+              text={"Generate Sketch"}
+              className={"ai-form-button"}
+            ></Button>
+            {status.message && status.success === true ? (
+              <h3 style={{ color: "green", marginBottom: "30px" }}>
+                {status.message}
+              </h3>
+            ) : (
+              <h3 style={{ color: "red", marginBottom: "30px" }}>
+                {status.message}
+              </h3>
+            )}
+          </form>
         )}
-        {!imageUrl && (
-          <div className="ai-sketch flex center column">
-            <p> There is no picture</p>
-          </div>
-        )}
+
+        <div className="sketch-container flex column center">
+          <h1>AI Sketch</h1>
+          {imageUrl && (
+            <>
+              <div className="ai-sketch">
+                <img src={imageUrl} alt="" />
+              </div>
+              <Button
+                type={"submit"}
+                name={"save-sketch"}
+                text={"Save Sketch"}
+                className={"ai-form-button"}
+              ></Button>
+            </>
+          )}
+          {!imageUrl && (
+            <div className="ai-sketch flex center column">
+              <p> There is no picture</p>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
