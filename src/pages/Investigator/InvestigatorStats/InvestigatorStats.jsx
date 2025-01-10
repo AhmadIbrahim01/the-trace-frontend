efde02b4d3c73667b5a13a4f4fb80d896c31a513
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2025-01-11 01:44:43 +0200

    Add: Data Fetching For Investigator Stats Page

--- src/pages/Investigator/InvestigatorStats/InvestigatorStats.jsx
+++ src/pages/Investigator/InvestigatorStats/InvestigatorStats.jsx
@@ -1,19 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./InvestigatorStats.css";
 import magnifierTwo from "../../../assets/images/magnifier-2.svg";
 import caribbeanFingerprint from "../../../assets/images/caribbean-fingerprint.svg";
+import axios from "axios";
+
+import { jwtDecode } from "jwt-decode";
 
 const InvestigatorStats = () => {
-  const data = {
-    total: "50",
-    solved: 35,
-    resolution: 75,
-    undergoing: 3,
-    cold: 12,
-  };
+  const [data, setData] = useState({
+    investigatorCaseCount: 0,
+    investigatorSolvedCasesCount: 0,
+    investigatorOpenCasesCount: 0,
+    investigatorClosedCasesCount: 0,
+  });
 
-  const { total, solved, resolution, undergoing, cold } = data;
+  const token = localStorage.getItem("authToken");
+  const [investigatorId, setInvestigatorId] = useState("");
 
+  useEffect(() => {
+    if (token) {
+      const decoded = jwtDecode(token);
+      setInvestigatorId(decoded.userId);
+    }
+  }, [token]);
+
+  useEffect(() => {
+    const getStats = async () => {
+      try {
+        const response = await axios.get(
+          `http://127.0.0.1:8080/api/investigator/stats/${investigatorId}`,
+          data,
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        setData(response.data);
+        console.log(data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getStats();
+  }, [investigatorId]);
+
+  const {
+    investigatorCaseCount,
+    investigatorSolvedCasesCount,
+    investigatorOpenCasesCount,
+    investigatorClosedCasesCount,
+  } = data;
+
+  const resolution =
+    (investigatorSolvedCasesCount / investigatorCaseCount) * 100;
   return (
     <>
       <div className="investigator-stats flex column center">
@@ -22,27 +63,27 @@ const InvestigatorStats = () => {
           Here you can find all your stats including number of cases, solved
           cases undergoing cases and much more informations
         </p>
-        <div class="investigator-grid">
-          <div class="div1 flex column center">
+        <div className="investigator-grid">
+          <div className="div1 flex column center">
             <p>Total Case Handled</p>
-            <h1>{total}</h1>
+            <h1>{investigatorCaseCount}</h1>
           </div>
-          <div class="div2 flex column center">
+          <div className="div2 flex column center">
             <p>Solved Cases</p>
-            <h1>{solved}</h1>
+            <h1>{investigatorSolvedCasesCount}</h1>
           </div>
-          <div class="div3 flex column center">
+          <div className="div3 flex column center">
             <p>Case Resolution Rate</p>
-            <h1>{resolution}%</h1>
+            <h1>{resolution.toFixed(1)}%</h1>
             <img src={caribbeanFingerprint} alt="" />
           </div>
-          <div class="div4 flex column center">
+          <div className="div4 flex column center">
             <p>Undergoing Cases</p>
-            <h1>{undergoing}</h1>
+            <h1>{investigatorOpenCasesCount}</h1>
           </div>
-          <div class="div5 flex column center">
+          <div className="div5 flex column center">
             <p>Cold Cases</p>
-            <h1>{cold}</h1>
+            <h1>{investigatorClosedCasesCount}</h1>
           </div>
         </div>
         <h3 className="t-center">
