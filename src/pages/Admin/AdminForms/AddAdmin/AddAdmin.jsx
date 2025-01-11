ae566fb9d3ab93e59f64fc520d7c9df5a0d53211
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2025-01-11 20:32:26 +0200

    Add: Add Admin Page

---
+++ src/pages/Admin/AdminForms/AddAdmin/AddAdmin.jsx
@@ -0,0 +1,155 @@
+import React, { useState } from "react";
+import Button from "../../../../components/Button/Button";
+import "./AddAdmin.css";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+const AddAdmin = () => {
+  const navigate = useNavigate();
+  const backTo = () => {
+    navigate("/manage-admins");
+  };
+  const [status, setStatus] = useState({ success: true, message: "" });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    const dataWithRole = { ...data, role: "admin" };
+
+    try {
+      const response = await axios.post(
+        `http://127.0.0.1:8080/api/auth/register`,
+        dataWithRole,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      setStatus({
+        success: true,
+        message: "Login successfull",
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.message);
+
+      const errorMessage =
+        error.response?.data?.message || "Something went wrong";
+      setStatus({ success: false, message: errorMessage });
+    }
+    reset();
+  };
+
+  return (
+    <div className="admin-form-container t-center flex column center">
+      <h1>Add Admins</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Button
+          name={"back"}
+          text={"Back to manage admins"}
+          className={"form-back-button"}
+          type={"button"}
+          onClick={backTo}
+        ></Button>
+
+        <div className="input flex column">
+          <label htmlFor="firstName">First Name</label>
+          <input
+            id="firstName"
+            name="firstName"
+            type="text"
+            {...register("firstName", { required: "First name is required" })}
+          />
+          {errors.firstName && (
+            <p style={{ color: "red" }}>{errors.firstName.message}</p>
+          )}
+        </div>
+        <div className="input flex column">
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            id="lastName"
+            name="lastName"
+            type="text"
+            {...register("lastName", { required: "Last name is required" })}
+          />
+          {errors.lastName && (
+            <p style={{ color: "red" }}>{errors.lastName.message}</p>
+          )}
+        </div>
+
+        <div className="input flex column">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            {...register("email", { required: "Email is required" })}
+          />
+          {errors.email && (
+            <p style={{ color: "red" }}>{errors.email.message}</p>
+          )}
+        </div>
+        <div className="input flex column">
+          <label htmlFor="phone">Phone Number</label>
+          <input
+            id="phone"
+            name="phone"
+            type="number"
+            {...register("phone", { required: "Phone is required" })}
+          />
+          {errors.phone && (
+            <p style={{ color: "red" }}>{errors.phone.message}</p>
+          )}
+        </div>
+        <div className="input flex column">
+          <label htmlFor="profilePicture">Profile Picture</label>
+          <input
+            id="profilePicture"
+            name="profilePicture"
+            type="file"
+            {...register("profilePicture", {
+              required: "Profile picture is required",
+            })}
+          />
+          {errors.profilePicture && (
+            <p style={{ color: "red" }}>{errors.profilePicture.message}</p>
+          )}
+        </div>
+
+        <div className="input flex column">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            {...register("password", {
+              required: "Password is required",
+              minLength: {
+                value: 6,
+                message: "Password must be at least 6 characters",
+              },
+            })}
+          />
+          {errors.password && (
+            <p style={{ color: "red" }}>{errors.password.message}</p>
+          )}
+        </div>
+
+        <Button
+          type={"submit"}
+          name={"add-investigator"}
+          text={"Add Admin"}
+          className={"ivestigator-form-button"}
+        ></Button>
+      </form>
+    </div>
+  );
+};
+
+export default AddAdmin;
